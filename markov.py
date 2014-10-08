fae952685481737dff4f11d9380744133ef063e9
Wendy Dherin <wendydherin@gmail.com>
2014-10-07 22:16:14 -0700

    Reworked comments for my pleasure.

--- markov.py
+++ markov.py
@@ -9,14 +9,19 @@ def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
 
-    exclude2 = "\n" + "\t"
     new_corpus = "" 
     
     for char in corpus:
-        if char in "_[]*":
+
+        # leave out certain kinds of punctuation
+        if char in "_[]*" or char == "--":
             continue
-        if char in exclude2:
-            new_corpus += " "  
+        
+        # replace newlines and tabs with a space
+        if char in "\n\t":
+            new_corpus += " "
+
+        # put everything else in the new_corpus string      
         else:
             new_corpus += char
 
@@ -24,6 +29,7 @@ def make_chains(corpus):
 
     d = {}
     for i in range( (len(list_of_words) - 2) ):
+
         prefix = (list_of_words[i], list_of_words[i+1])
         suffix = list_of_words[i+2] 
         
@@ -35,42 +41,46 @@ def make_chains(corpus):
     # add link from end of wordlist to beginning of wordlist
     prefix = (list_of_words[-2], list_of_words[-1])
     suffix = list_of_words[0]
-    ## print suffix
     d[prefix] = suffix
 
-    ## print d
     return d
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    # generate a one-time random prefix
+    # create a list of chain's keys, then return one of the keys at random
     random_prefix = random.choice(chains.keys())
 
-    # get random suffix from the list
+    # from the list of values for the chosen key, return one value at random
     random_suffix = random.choice(chains[random_prefix])
+    
+    # initialize an empty string for our random text string
     markov_text = ""
 
-    # 
+    # iterate over prefix's tuple and add each word to the random text string
     for word in random_prefix:
         markov_text += word + " "
+
+    # then add the suffix
     markov_text += random_suffix + " "
 
+    # rename random_prefix and random_suffix so that we can call them
+    # in a the following for loop
     prefix = random_prefix
     suffix = random_suffix
-    newprefix = (prefix[1], suffix)
-    newsuffix = chains[newprefix]
-    random_new_suffix = random.choice(newsuffix)
-
 
     for i in range(100):
+
+        # create a new prefix from the last item in the old prefix and
+        # the last suffix
         prefix = (prefix[1], suffix)
 
+        # choose a random suffix from the new prefix's values
         suffix = random.choice(chains[prefix])
 
-        markov_text += "%s %s %s" % (prefix[0], prefix[1], suffix)
-        markov_text += " "
+        # add it all to the random text string
+        markov_text += "%s %s %s " % (prefix[0], prefix[1], suffix)
 
     return markov_text
 
